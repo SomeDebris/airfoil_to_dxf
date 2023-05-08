20804e31777c5b282308292a3c07bdba32536b17
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-05-08 13:26:52 -0700

    main: add basic code for reading arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs;
+
+fn array_to_dxf() {
+        
+}
+
+struct Config {
+    airfoil_filename: String,
+    output_chord_length: f64,
+}
+
+fn parse_arguments(args: &[String]) -> Result<Config, &'static str> {
+    let airfoil_filename = args[1].clone();
+    let output_chord_length: f64 = args[2].clone().parse().unwrap();
+
+    Ok( Config{ airfoil_filename, output_chord_length } )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    let config = parse_arguments(&args);
 }
