0562ed80aaa1fc65706a753d62a87817334fc799
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-05-08 18:07:34 -0700

    main: make parse_arguments handle more situations
    There are now more checks done on arguments passed to the function.

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,17 @@ fn parse_arguments(args: &[String]) -> Result<Config, &'static str> {
         return Err("Not enough arguments!");
     }
 
+    let output_chord_length: f64 = if args.len() < 2 { 1.0 } else { 
+        args[2].clone().parse().expect("Second argument should be a float!") };
+
     let airfoil_filename = args[1].clone();
-    let output_chord_length: f64 = args[2].clone().parse().expect("Second argument should be a float!");
 
     Ok( Config{ airfoil_filename, output_chord_length } )
 }
 
 fn main() {
+    println!("Make sure your airfoil uses Selig format!");
+
     let args: Vec<String> = env::args().collect();
     
     let config = parse_arguments(&args);
