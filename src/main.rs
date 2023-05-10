1932458a4caecc077b0875617d3c25518178fc82
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2023-05-10 16:01:50 -0700

    main.rs: make a constructor for Config

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,25 @@ struct Config {
     output_chord_length: f64,
 }
 
-fn parse_arguments(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 2 {
-        return Err("Not enough arguments!");
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments!");
+        }
+
+        let no_chord_length_given: bool = args.len() < 3;
+
+        if no_chord_length_given {
+            println!("No chord length given. Assuming chord length of 1.");
+        }
+
+        let output_chord_length: f64 = if no_chord_length_given { 1.0 } 
+            else { args[2].clone().parse().expect("Second argument should be a float!") };
+
+        let airfoil_filename = args[1].clone();
+
+        Ok(Config { airfoil_filename, output_chord_length }) 
     }
-
-    let no_chord_length_given: bool = args.len() < 3;
-
-    if no_chord_length_given {
-        println!("No chord length given. Assuming chord length of 1.");
-    }
-
-    let output_chord_length: f64 = if no_chord_length_given { 1.0 } 
-        else { args[2].clone().parse().expect("Second argument should be a float!") };
-
-    let airfoil_filename = args[1].clone();
-
-    Ok( Config{ airfoil_filename, output_chord_length } )
 }
 
 fn main() {
@@ -35,4 +37,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = parse_arguments(&args);
+
+    let airfoil_file_contents = fs::read_to_string(config.airfoil_filename)
+        .expect("Should have been able to read file!");
 }
